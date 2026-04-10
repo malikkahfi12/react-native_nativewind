a037fbfc43ca7b6e44e6b6a526895fff1970db20
malikkahfi12 <malikkahfi1996@gmail.com>
2026-04-10 22:49:29 +0700

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {
-      colors: {
-        background: "#fff9e3",
-        foreground: "#081126",
-        card: "#fff8e7",
-        muted: "#f6eecf",
-        "muted-foreground": "rgba(0, 0, 0, 0.6)",
-        primary: "#081126",
-        accent: "#ea7a53",
-        border: "rgba(0, 0, 0, 0.1)",
-        success: "#16a34a",
-        destructive: "#dc2626",
-        subscription: "#8fd1bd",
-      },
-      spacing: {
-        0: "0px",
-        1: "4px",
-        2: "8px",
-        3: "12px",
-        4: "16px",
-        5: "20px",
-        6: "24px",
-        7: "28px",
-        8: "32px",
-        9: "36px",
-        10: "40px",
-        11: "44px",
-      },
+    // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+    content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {
+            colors: {
+                background: "#fff9e3",
+                foreground: "#081126",
+                card: "#fff8e7",
+                muted: "#f6eecf",
+                "muted-foreground": "rgba(0, 0, 0, 0.6)",
+                primary: "#081126",
+                accent: "#ea7a53",
+                border: "rgba(0, 0, 0, 0.1)",
+                success: "#16a34a",
+                destructive: "#dc2626",
+                subscription: "#8fd1bd",
+            },
+            fontFamily: {
+                "sans": "sans-regular",
+                "sans-light": "sans-light",
+                "sans-mediun": "sans-medium",
+                "sans-semibold": "sans-semibold",
+                "sans-bold": "sans-bold",
+                "sans-extrabold": "sans-extrabold"
+            },
+            spacing: {
+                0: "0px",
+                1: "4px",
+                2: "8px",
+                3: "12px",
+                4: "16px",
+                5: "20px",
+                6: "24px",
+                7: "28px",
+                8: "32px",
+                9: "36px",
+                10: "40px",
+                11: "44px",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
